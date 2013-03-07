6bc0daab25ff9b32afe405b0d026d329235c96ba
regueiro <santi@regueiro.es>
2013-03-07 21:36:40 +0100

    Prototype collection controller

---
+++ src/test/java/es/regueiro/collectionManager/controller/CollectionManagerTest.java
@@ -0,0 +1,40 @@
+package es.regueiro.collectionManager.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CollectionManagerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+				"collection-beans.xml");
+		
+		CollectionManager manager = (CollectionManager) context.getBean("collectionManager");
+		
+		manager.testCollection();
+	}
+
+}
